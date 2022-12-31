50261d082d41b983fd346f0af9406b8936895f2b
sajibdigital <sakifraihan@gmail.com>
2022-12-31 21:57:08 +0600

    first $ git commit -m first

---
+++ 79_function_recap.py
@@ -0,0 +1,16 @@
+def square(a):
+    """
+    Return square of a number
+    """
+    double = a * a
+    return double
+
+# print(square(20))
+# print(square.__doc__)
+# print(len.__doc__)
+
+def multiplication(a,b):
+    multi = a * b
+    return multi
+
+print(multiplication(2,3))
